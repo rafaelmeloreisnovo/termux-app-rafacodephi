773d4737bc864b7a39e40f23a42b91436f788d84
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-04-08 20:39:56 -0300

    Align rmr linker flags with 16KB page-size modules

--- rmr/src/main/cpp/Android.mk
+++ rmr/src/main/cpp/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := rmr
 LOCAL_SRC_FILES := rmr.c
 LOCAL_CFLAGS := -std=c11 -Wall -Wextra -Werror -Os -fno-stack-protector
-LOCAL_LDFLAGS := -Wl,--gc-sections
+# Necessário para compatibilidade com dispositivos de page size 16KB
+LOCAL_LDFLAGS := -Wl,--gc-sections -Wl,-z,max-page-size=16384
 
 include $(BUILD_SHARED_LIBRARY)
