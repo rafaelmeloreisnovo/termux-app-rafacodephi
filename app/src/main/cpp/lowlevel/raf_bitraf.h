487e75a7723ae45383f54f9d717991e898f3f963
wojcikiewicz17 <pereirajulio1985@gmail.com>
2026-05-05 04:06:49 -0300

    Refactor BitRAF debug string into optional module

--- app/src/main/cpp/lowlevel/raf_bitraf.h
+++ app/src/main/cpp/lowlevel/raf_bitraf.h
@@ -5,6 +5,8 @@
 uint64_t bitraf_encode(uint8_t opcode, uint8_t dir, uint16_t layer, uint16_t imm, uint16_t flags);
 void bitraf_decode(uint64_t insn, uint8_t* opcode, uint8_t* dir, uint16_t* layer, uint16_t* imm, uint16_t* flags);
 int bitraf_validate(uint64_t insn);
+#if !defined(RMR_NO_DEBUG_STRING)
 int bitraf_to_string(uint64_t insn, char* out, int cap);
+#endif
 
 #endif
